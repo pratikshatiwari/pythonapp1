58bc35faa46cfc2c5ac50db2cb54bfea179de5fc
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-26 01:18:05 +0530

    Create nonexistencefunctions.py

---
+++ nonexistencefunctions.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# Correct function calls
+result1 = add(10, 5)
+result2 = subtract(10, 5)
+
+# Incorrect function call: `multiply` is not defined
+result3 = multiply(10, 5)
+
+# Another correct function call
+result4 = add(20, 10)
+
+print("Results:", result1, result2, result4)
