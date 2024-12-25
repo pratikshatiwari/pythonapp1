44c6f65f448f4074e34474d93756f5efc4e9eab6
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-26 01:11:06 +0530

    Create incorrectfunction.py

---
+++ incorrectfunction.py
@@ -0,0 +1,9 @@
+import math
+
+def add(a, b):
+    return a + b
+
+# Incorrect function calls
+result = add(1)  # Missing an argument
+value = math.sine(30)  # Non-existent function
+output = add("1", 2)  # Type mismatch
